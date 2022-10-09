8d5ca8023b58daa9f39a174e5dd47da68d8d2249
Srikanth Reddy <bandarisrikanth741@gmail.com>
2022-10-09 08:19:08 +0530

    [ srikanth |Added JWT authontication and implemented swagger]

--- RepositoryLayer/Service/UserRL.cs
+++ RepositoryLayer/Service/UserRL.cs
@@ -48,6 +48,27 @@ namespace RepositoryLayer.Service
                 throw ex;
             }
         }
+        private string GetJWTToken(string email, long userID)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.ASCII.GetBytes(configuration["key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("email", email),
+                    new Claim("userID",userID.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+
+                SigningCredentials =
+                new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
 
